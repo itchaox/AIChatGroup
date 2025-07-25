5ec4e167e3c39115aeb1b2c10cb4bbd204ff1871
Wang Chao <wangchao2423@gmail.com>
2025-07-25 10:37:23 +0800

    style(AIToolSelector): 调整设置图标大小从w-3 h-3到w-4 h-4

--- src/components/AIToolSelector.tsx
+++ src/components/AIToolSelector.tsx
@@ -122,7 +122,7 @@ const AIToolSelector: React.FC = () => {
                        className="absolute right-2 top-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100 p-1 rounded hover:bg-gray-200 dark:hover:bg-gray-600 transition-all"
                        title="编辑工具"
                      >
-                       <Settings className="w-3 h-3 text-gray-500 dark:text-gray-400" />
+                       <Settings className="w-4 h-4 text-gray-500 dark:text-gray-400" />
                      </button>
                    </div>
                  );
